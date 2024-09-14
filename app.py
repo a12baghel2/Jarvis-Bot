bceb7c04da88d7549d214bf2ff4a917a46f0e8d6
Abhimanyu <43077595+a12baghel2@users.noreply.github.com>
2024-09-14 21:08:05 +0000

    some fixes

--- app.py
+++ app.py
@@ -33,41 +33,41 @@ def camera(bot,update):
 def news(bot,update):
 	bot.send_message(chat_id=update.message.chat_id,text="Choose a category",reply_markup=ReplyKeyboardMarkup(keyboard=news_topic,one_time_keyboard=True))
 
-def reply_text(bot,update):
+async def reply_text(bot,update):
 	intent,reply = get_reply(update.message.text,update.message.chat_id)
 	print(intent)
 	if intent == "corona status":
 		status = fetch_status(reply)
-		bot.send_message(chat_id=update.message.chat_id,text=status)
+		await bot.send_message(chat_id=update.message.chat_id,text=status)
 	if intent == "show climate":
 		weather = fetch_weather(reply)
 		print(reply.get('geo-city'))
-		bot.send_message(chat_id=update.message.chat_id,text=weather)
+		await bot.send_message(chat_id=update.message.chat_id,text=weather)
 	if intent == "get_quotes":
 		quote = fetch_quote(reply)
-		bot.send_message(chat_id=update.message.chat_id,text=quote)
+		await bot.send_message(chat_id=update.message.chat_id,text=quote)
 	if intent == "mars":
 		get_info = mars(reply)
 		for i in get_info['photos'][:5]:
 			sendReply = "Rover Name: {}\nLaunch Date: {}\nLanding Date: {}\nCamera Name: {}\nPicture: {}".format(i['rover']['name'],i['rover']['launch_date'],i['rover']['landing_date'],i['camera']['full_name'],i['img_src'])
-			bot.send_message(chat_id=update.message.chat_id,text=sendReply)
+			await bot.send_message(chat_id=update.message.chat_id,text=sendReply)
 	
 	if intent == "planet_pic":
 		pic = getPic(reply)
 		sendReply = "Date: {}\nCopyright: {}\nDescription:\n{}\nImage: {}".format(pic['date'],pic['copyright'],pic['explanation'],pic['hdurl'])
-		bot.send_message(chat_id=update.message.chat_id,text=sendReply)
+		await bot.send_message(chat_id=update.message.chat_id,text=sendReply)
 
 	if intent == "get_news":
 		news = get_news(reply)
 		for i in news[:10]:
 			sendReply = "Title: {}\nDescription: {}\nAuthor: {}\nLink: {}".format(i['title'],i['description'],i['author'],i['url'])
-			bot.send_message(chat_id=update.message.chat_id,text=sendReply)
+			await bot.send_message(chat_id=update.message.chat_id,text=sendReply)
 
 	if intent ==  "small_talk":
-		bot.send_message(chat_id=update.message.chat_id,text=reply)
+		await bot.send_message(chat_id=update.message.chat_id,text=reply)
 
-def echo_sticker(bot,update):
-	bot.send_sticker(chat_id=update.message.chat_id,sticker=update.message.sticker.file_id)
+async def echo_sticker(bot,update):
+	await bot.send_sticker(chat_id=update.message.chat_id,sticker=update.message.sticker.file_id)
 
 def error(bot,update):
 	logger.error("update '%s' caused error '%s'",update,update.error)
@@ -75,9 +75,10 @@ def error(bot,update):
 bot = Bot(TOKEN)
 
 try:
-	async def set_webhook(): await bot.set_webhook("https://jarvis-bot-omega.vercel.app/"+TOKEN)
+	async def set_webhook(): await dp.bot.set_webhook("https://jarvis-bot-omega.vercel.app/"+TOKEN)
 except Exception as e:
 	print(e)
+
 
 dp = Application.builder().token(TOKEN).build()
 dp.add_handler(CommandHandler("start",start))
@@ -87,7 +88,8 @@ dp.add_handler(CommandHandler("news",news))
 dp.add_handler(MessageHandler(filters.Text,reply_text))
 dp.add_handler(MessageHandler(filters.Sticker,echo_sticker))
 dp.add_error_handler(error)
-dp.run_webhook("https://jarvis-bot-omega.vercel.app/", key=TOKEN)
+set_webhook()
+dp.run_polling(0.3)
 
 
 
